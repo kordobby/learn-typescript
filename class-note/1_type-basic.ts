4720041d1d79859e6e8f7a01b1f6520d2c343ded
lxxtrue <poordobby@naver.com>
2023-07-16 22:02:43 +0900

    add. 타입선언

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,10 @@
+var str = "hello";
+var str: string = "hello";
+
+let num: number = 10;
+let arr: Array<number> = [1, 2, 3];
+let pokemons: Array<string> = ["피카츄", "파이리", 0]; // ERROR
+pokemons = [0, 1, 2];
+pokemons = ["피카츄", "파이리"];
+
+let hogwarts: string[] = ["해리포터", "론", "헤르미온느"];
