a20aa2af7e750943e1a22f1c667663e05ee8027d
lxxtrue <poordobby@naver.com>
2023-07-24 01:06:57 +0900

    typescript setting

---
+++ getting-started/index.js
@@ -0,0 +1,5 @@
+// npm i typescript -g
+function sum(a, b) {
+    return a + b;
+}
+sum(10, 20);
---
+++ getting-started/index.ts
@@ -0,0 +1,8 @@
+// npm i typescript -g
+// tsc => javascript 로 변환
+// tsc index.ts
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+sum(10, 20);
